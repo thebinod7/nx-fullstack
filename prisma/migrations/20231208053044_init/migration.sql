8f6a7247b4a0f406a38b7269fdb6353bfeb8b08f
Binod Chaudhary <thebinod7@gmail.com>
2023-12-08 11:16:44 +0545

    add prisma db and migration

---
+++ prisma/migrations/20231208053044_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "AuthType" AS ENUM ('Email', 'Phone', 'Wallet');
+
+-- CreateTable
+CREATE TABLE "roles" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "isSystem" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3)
+);
+
+-- CreateTable
+CREATE TABLE "permissions" (
+    "id" SERIAL NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "action" VARCHAR NOT NULL,
+    "subject" VARCHAR NOT NULL,
+    "inverted" BOOLEAN NOT NULL DEFAULT false,
+    "conditions" JSONB,
+    "reason" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3)
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authAddress" TEXT NOT NULL,
+    "authType" "AuthType" NOT NULL DEFAULT 'Email',
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "roleId" INTEGER NOT NULL,
+    "otp" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_id_key" ON "roles"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_name_key" ON "roles"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "permissions_id_key" ON "permissions"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_authAddress_key" ON "users"("authAddress");
+
+-- AddForeignKey
+ALTER TABLE "permissions" ADD CONSTRAINT "permissions_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
